c053b36b93c16d4f0e936e1c387adc15bd4944ab
chaospie <chaospie@cloud.(none)>
2013-01-20 22:52:39 +0000

    Updated commets in haskell solution to problem one

--- puzzles/project-euler/oneEuler.hs
+++ puzzles/project-euler/oneEuler.hs
@@ -4,11 +4,16 @@
     Find the sum of all the multiples of 3 or 5 below 1000.
 -}
 
--- sums the multiples of 3 and 5 in the list "xs" 
+{-
+    first (naive) solution
+    sums the multiples of 3 and 5 in the list "xs" 
+-}
 sumIfNotMultiples :: [Int] -> Int
 sumIfNotMultiples xs = sum [x | x <- xs, (mod x 3 == 0) || (mod x 5 == 0) && (mod x 15) /= 0 ]
 
--- Closed solution (from helix7 project euler solution (python) forum post in problem)
+{- 
+    closed solution (see codeplayground/latex/project-euler/projectEuler.tex for in-depth analysis)
+-}
 pe1 :: Integer -> Integer -> Integer -> Integer
 pe1 a b n = (f a n  + f b n  - f (a*b) n) `div` 2
     where f x y = x * ((y-1) `div` x) * (((y-1) `div` x) + 1)
