9fbc83378bd9ffa4cd05b4b8d77127ee9138ec09
wayofthepie <wayofthepie@gmail.com>
2013-01-26 14:09:37 +0000

    Added description to basic-server.js

--- javascript/nodePlayground/basic-server.js
+++ javascript/nodePlayground/basic-server.js
@@ -1,3 +1,7 @@
+/** 
+ * A basic server which accepts connections at port 4000 and saves any received
+ * data in logs.json.
+ */
 var net = require('net');
 var file = require('fs').createWriteStream(__dirname + '/logs.json');
 var server = net.createServer();
