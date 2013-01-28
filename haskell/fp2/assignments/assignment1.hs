123866b49bb4d8910d7272537740cc5e058e4dd9
wayofthepie <wayofthepie@gmail.com>
2013-01-28 23:27:13 +0000

    Updating assignment1.hs to actual assignment submitted.

--- haskell/fp2/assignments/assignment1.hs
+++ haskell/fp2/assignments/assignment1.hs
@@ -8,19 +8,24 @@ numLeaves EmptyBST              = 0
 numLeaves (Node lSub _ rSub)    | (isEmptyBST lSub && isEmptyBST rSub ) = 1
                                 | otherwise = numLeaves lSub + numLeaves rSub
 
+
 {-
     2. minValue bst : minimum value in the BST "bst"
--}
+-}                 
 minValue :: Ord a => BST a -> a
 minValue (Node lSub x _)    | (isEmptyBST lSub) = x
                             | otherwise = minValue lSub
+minValue EmptyBST           = error "BST is empty!"
+
 
 {-
     3. maxValue bst : maximum value in the BST "bst"
 -}
 maxValue :: Ord a => BST a -> a
-maxValue (Node _ x rSub) | (isEmptyBST rSub) = x
-                         | otherwise = maxValue rSub    
+maxValue (Node _ x rSub)    | (isEmptyBST rSub) = x
+                            | otherwise = maxValue rSub   
+maxValue EmptyBST           = error "BST is empty!"
+
 
 {-
     4. height bst : height of the BST "bst"
@@ -28,6 +33,7 @@ maxValue (Node _ x rSub) | (isEmptyBST rSub) = x
 height :: Ord a => BST a -> Int
 height EmptyBST             = 0
 height (Node lSub _ rSub)   = max (1 + height lSub) (1 + height rSub)
+
 
 {-
     5. occurs v bst : if value "v" occurs in the BST "bst"
@@ -38,12 +44,14 @@ occurs v (Node lSub x rSub) | v < x     = occurs v lSub
                             | otherwise = True
 occurs _ EmptyBST           = False
 
+
 {-
     6. mirror bst : a mirror image of the BST "bst"
 -}
 mirror :: Ord a => BST a -> BST a
 mirror EmptyBST = EmptyBST
 mirror (Node lSub x rSub) = Node (mirror rSub) x (mirror lSub)
+
 
 {-
     7. treeSort list : the list "list" sorted into ascending order
@@ -63,17 +71,21 @@ insert v (Node lSub root rSub)  = if v < root then
                                   else
                                     Node lSub root (insert v rSub)
 
+                                    
 -- leaf v : the single-node BST storing the value "v"
 leaf :: Ord a => a -> BST a 
 leaf v = Node EmptyBST v EmptyBST
+
+
+-- isEmptyBST bst : whether the BST "bst" is an EmptyBST
+isEmptyBST :: Ord a => BST a -> Bool
+isEmptyBST EmptyBST   = True
+isEmptyBST _          = False
+
 
 -- bstToList bst : the list formed after an in order traversal of the BST "bst"
 bstToList :: Ord a => BST a -> [a]
 bstToList EmptyBST                      = []
 bstToList (Node EmptyBST x EmptyBST)    = [x]
-bstToList (Node lSub x rSub)            = bstToList lSub ++ x:bstToList rSub
-   
--- isEmptyBST bst : whether the BST "bst" is an EmptyBST
-isEmptyBST :: Ord a => BST a -> Bool
-isEmptyBST EmptyBST   = True
-isEmptyBST _          = False
+bstToList (Node lSub x rSub)            = bstToList lSub ++ x : bstToList rSub
+
